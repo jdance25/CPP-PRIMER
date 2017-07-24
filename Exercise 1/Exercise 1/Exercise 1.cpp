ed6e0c9a5b8a150b3b84eb6dbe70634487de1177
jdance25 <matthewjpipes@gmail.com>
2017-07-24 15:59:07 -0400

    Exercise 1

---
+++ Exercise 1/Exercise 1/Exercise 1.cpp
@@ -0,0 +1,16 @@
+// Exercise 1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 1;
+	cout << i;
+
+	system("pause");
+    return 0;
+}
+
